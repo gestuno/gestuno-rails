4379d17860a1d9c4160c57db8ae07b41f4fefef7
Sophie Li <sophcli@gmail.com>
2018-11-27 20:36:40 +0800

    finished endcall page - located in pages_controller for now & fixed navbar structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :endcall]
 
   def home
+  end
+
+  def endcall
   end
 end
