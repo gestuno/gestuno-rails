59d717c00b232859f2e2a40ca2796eee5fdc0ebf
Sophie Li <sophcli@gmail.com>
2018-11-28 19:55:37 +0800

    need to complete MCV

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,18 +1,23 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [ :show ]
-  
+
   def index
     @customers = User.all.select { |user| user.customer? && user.online? }
     # raise
   end
 
   def show
+    @customer = User.find(set_customer)
   end
 
-  private
-  
-  def set_customer
-    @customer = User.find(params[:id])
-  end
+  def
 
-end
+
+
+    private
+
+    def set_customer
+      @customer = User.find(params[:id])
+    end
+
+  end
