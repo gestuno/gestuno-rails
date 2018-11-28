41585d52c6a0a65c5ce1f7c4163cfc2171f8c86f
Armani <armanivvv@gmail.com>
2018-11-28 12:32:33 +0800

    Corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'interpreters/show'
-  get 'interpreters/index'
-  get 'customers/show'
+  # get 'interpreters/show'
+  # get 'interpreters/index'
+  # get 'customers/show'
   # TODO - delete this temp route once code is in correct file
   get 'pages/endcall'
   root to: 'homes#home' # redirects to other pages depending on user type/login status
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users # includes [edit, new, create, update, destroy] for all types of profile
+  devise_for :users  # includes [edit, new, create, update, destroy] for all types of profile
+  devise_scope :user do
+    get 'profile', to: 'devise/registrations#edit'
+  end
   resources :customers, only: [:show, :index]
   resources :interpreters, only: [:show, :index]
 
