fba3ad7c9ab5162252699f03f456ca1eb8f04e60
lionel-rowe <lionel.rowe@gmail.com>
2018-11-29 18:57:07 +0800

    Update 20181129072629_add_language_to_interpreter_profile.rb

--- db/migrate/20181129072629_add_language_to_interpreter_profile.rb
+++ db/migrate/20181129072629_add_language_to_interpreter_profile.rb
@@ -1,5 +1,6 @@
 class AddLanguageToInterpreterProfile < ActiveRecord::Migration[5.2]
   def change
     add_column :interpreter_profiles, :language, :string
+    # TODO add model validation from preselected list
   end
 end
