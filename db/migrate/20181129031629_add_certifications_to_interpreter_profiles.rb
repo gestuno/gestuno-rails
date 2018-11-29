05e4c9eef28c08bdbc407c17d63ec33a4b56c7ce
lionel-rowe <lionel.rowe@gmail.com>
2018-11-29 18:56:50 +0800

    Update 20181129031629_add_certifications_to_interpreter_profiles.rb

--- db/migrate/20181129031629_add_certifications_to_interpreter_profiles.rb
+++ db/migrate/20181129031629_add_certifications_to_interpreter_profiles.rb
@@ -1,5 +1,6 @@
 class AddCertificationsToInterpreterProfiles < ActiveRecord::Migration[5.2]
   def change
     add_column :interpreter_profiles, :certifications, :string
+    # TODO add model validation from preselected list
   end
 end
