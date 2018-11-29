765424db8a829ce4ef45c448cafaf4b3390386a7
Sophie Li <sophcli@gmail.com>
2018-11-29 12:33:18 +0800

    Added user model reference to customer profiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_041338) do
+ActiveRecord::Schema.define(version: 2018_11_29_042227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_11_29_041338) do
     t.string "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_customer_profiles_on_user_id"
   end
 
   create_table "interpreter_profiles", force: :cascade do |t|
@@ -54,5 +56,6 @@ ActiveRecord::Schema.define(version: 2018_11_29_041338) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "customer_profiles", "users"
   add_foreign_key "interpreter_profiles", "users"
 end
