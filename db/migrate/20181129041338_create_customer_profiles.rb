4788634d9c3102c06c647a1750d7200742f9e366
lionel-rowe <lionel.rowe@gmail.com>
2018-11-29 22:46:04 +0800

    Delete 20181129041338_create_customer_profiles.rb

--- db/migrate/20181129041338_create_customer_profiles.rb
+++
@@ -1,9 +0,0 @@
-class CreateCustomerProfiles < ActiveRecord::Migration[5.2]
-  def change
-    create_table :customer_profiles do |t|
-      t.string :language
-
-      t.timestamps
-    end
-  end
-end
