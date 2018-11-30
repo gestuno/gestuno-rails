9077d9c02db6aeb1ff2c3809db0d6ec18cb6eba8
Lionel Rowe <lionel.rowe@gmail.com>
2018-11-30 21:26:33 +0800

    create reusable avatar component and helper methods

--- app/helpers/utils.rb
+++ app/helpers/utils.rb
@@ -1,5 +1,36 @@
 module Utils
-  def self.email_to_name(email)
-    email.gsub(/@.+/, '').split(/[-_.]/).map { |word| word.capitalize } .join(' ')
+
+  class << self
+
+    def user_to_name(user)
+      user.email.gsub(/@.+/, '').split(/[-_.]/).map { |word| word.capitalize } .join(' ')
+    end
+
+    def user_to_initials(user)
+      user_name = self.user_to_name(user)
+
+      user_name.split(/\s+/).map { |word| word[0].upcase } .join('')[0..2]
+    end
+
+    def str_to_bg_and_fg_color(str)
+      digest = Digest::SHA256.hexdigest(str).to_i(16)
+
+      color = '#' + Random.new(digest).rand(0..16777215).to_s(16).rjust(6, '0')
+
+      r = color[1..2].to_i(16)
+      g = color[3..4].to_i(16)
+      b = color[5..6].to_i(16)
+
+      lum = (0.299 * r + 0.587 * g + 0.114 * b) / 255
+
+      fg = lum > 0.6 ? '#333' : '#eee'
+
+      {
+        bg: color,
+        fg: fg
+      }
+    end
+
   end
+
 end
