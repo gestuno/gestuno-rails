0715c4c88393f927eed36d2abdbfafb66b226344
Armani <armanivvv@gmail.com>
2018-12-03 12:29:50 +0800

    Fili / Marine customer fixes based on Sergios implementation

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,40 @@
 class CustomersController < BaseUsersController
+  before_action :set_user, only: [:show, :create, :update]	
+  # def index	
+  #   @customers = User.where(customer: true).select { |i| i.online? } # TODO improve performance	
+  # end	
+  
+  def show
+    @user = User.find(params[:id])
+    @customer = @user.customer_profile
+  end	
+  
+  def new	
+    @customer = customerProfile.new	
+  end	
+  
+  def edit	
+    @user = User.find(params[:id])
+    @customer = @user.customer_profile	
+  end	
 
-  private
-
-  def user_type
-    :customer
+  def create	
+    @customer = customerProfile.new(customer_params)	
+    @customer.user = current_user	
+    @customer.save	
+    redirect_to :root	
   end
 
-  def allowed_params
-    # params.require(:customer).permit(:language, :bio, :gender)
-    params.require(:customer).permit(:gender, :language, :avatar)
+  def update	
+    @customer.update(customer_params)	
+    redirect_to :root
+  end
+
+  def set_user
+    @customer = current_user.customer_profile
+  end
+
+  def customer_params	 
+    params.require(:customer).permit(:gender, :language, :avatar, :bio)	   
   end
 end
