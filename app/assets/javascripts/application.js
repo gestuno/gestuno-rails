a2e77d818178fc55d63226169abfea48a96a08ad
Lionel Rowe <lionel.rowe@gmail.com>
2018-12-03 22:04:32 +0800

    in-browser modal (sweetalert) to allow user to join incoming call

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,18 +7,31 @@
 //= require_tree .
 //= require cable
 
+//= require sweetalert2
 
+// notifications_#{current_user.id}"
 
-
-
+// const swal = require('sweetalert');
 
 function SubscribeChannel(){
-  App.cable.subscriptions.create(
-  {channel: 'NotificationsChannel'},
+  App.cable.subscriptions.create({ channel: 'NotificationsChannel' },
   {
-    received: function(data) {
-      alert("hey!");
-      console.log(data);
+    received: data => {
+      // if (confirm(`Incoming call from ${data.senderName}. Accept?`)) {
+      //   window.location = `/join?room=${data.roomName}`;
+      // }
+
+      swal({
+        title: "Incoming call",
+        text: `Incoming call from ${data.senderName}. Accept?`,
+        showCancelButton: true,
+        confirmButtonText: 'Accept',
+        cancelButtonText: 'Decline'
+      })
+      .then(accepted => {
+         if (accepted) window.location = `/join?room=${data.roomName}`;
+      });
+
     }
   });
 }
