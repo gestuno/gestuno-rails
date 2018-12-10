1dd47b8ba5377f87950c5d68c72814cf47a4713d
Lionel Rowe <lionel.rowe@gmail.com>
2018-12-11 00:05:46 +0800

    add connecting animation on outgoing call start; styling tweaks; disable registrations; fix bug with call button showing on own profile when current_user is interpreter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,12 @@ class PagesController < ApplicationController
   def aboutus
   end
 
+  def registrations_disabled
+    render html: <<-HTML
+      <div>Sorry! Registrations are disabled at the moment.</div>
+      <a href='/'>Home</a>
+    HTML
+      .html_safe
+  end
+
 end
